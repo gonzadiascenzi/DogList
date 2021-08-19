6d6e97b6ff68a32eb396f5cf6873fa50f3a2f0b4
gonzadiascenzi <gonzalo.diascenzi@globant.com>
2021-08-19 20:04:47 -0300

    Configurando response Creando interfaz (configurando metodo de acceso al API)

--- app/src/main/java/com/example/doglistbeta/DogResponse.kt
+++ app/src/main/java/com/example/doglistbeta/DogResponse.kt
@@ -1,4 +1,8 @@
 package com.example.doglistbeta
 
-class DogResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+class DogResponse (@SerializedName ("status") var status: String,
+                   @SerializedName ("message") var images: List<String>
+
+                   )

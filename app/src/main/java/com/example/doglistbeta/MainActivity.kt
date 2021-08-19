df88c97c154dedeb41d7d4ecce1b863346b0f789
gonzadiascenzi <gonzalo.diascenzi@globant.com>
2021-08-19 20:30:53 -0300

    Configurando funciones MainActivity.kt

--- app/src/main/java/com/example/doglistbeta/MainActivity.kt
+++ app/src/main/java/com/example/doglistbeta/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.doglistbeta
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.doglistbeta.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +18,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
+    private fun searchByName (query:String){ //En la variable query de tipo string llegara los datos que escriba el usuario
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()
+            if(call.isSuccessful){
+                //Mostramos recyclerview
+            }else{
+                //No mostramos recyclerview
+            }
 
+        }
+    }
 
 }
